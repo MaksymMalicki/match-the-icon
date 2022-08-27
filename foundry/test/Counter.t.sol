aaccbb98ce92d1c8dc77ddab61b2b6840df97b22
Kamil Chmielewski <45183584+ChmielewskiKamil@users.noreply.github.com>
2022-08-28 01:05:06 +0200

    Game contract development (#4)
    * build: add .gitignore to the main project folder
    
    .gitignore is needed for both angular frontend and foundry
    in order for it to work properly it needs to be visible by both
    projects
    
    * test: add scaffold for the test suite
    
    * feat: create MatchingGame contract
    
    * test: add import of the game contract
    
    * build: delete unused files
    
    * test(game-instance): add variables to setUp
    
    * test: add hashed answer
    
    * test: delete test
    
    * delete everything
    
    * build: fix foundry
    
    * build: delete foundry
    
    * build: forge init

--- foundry/test/Counter.t.sol
+++ foundry/test/Counter.t.sol
@@ -6,9 +6,18 @@ import "../src/Counter.sol";
 
 contract CounterTest is Test {
     Counter public counter;
-
     function setUp() public {
-        counter = new Counter();
-        counter.setNumber(0);
+       counter = new Counter();
+       counter.setNumber(0);
+    }
+
+    function testIncrement() public {
+        counter.increment();
+        assertEq(counter.number(), 1);
+    }
+
+    function testSetNumber(uint256 x) public {
+        counter.setNumber(x);
+        assertEq(counter.number(), x);
     }
 }
