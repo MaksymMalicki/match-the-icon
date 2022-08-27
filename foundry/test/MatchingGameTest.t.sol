f8d56a62c4da49b703c302294d5406c957ba4fc8
Kamil Chmielewski <chmielewski.kamil45@gmail.com>
2022-08-27 23:09:32 +0200

    test(game-instance): add variables to setUp

--- foundry/test/MatchingGameTest.t.sol
+++ foundry/test/MatchingGameTest.t.sol
@@ -4,4 +4,15 @@ pragma solidity 0.8.16;
 import "forge-std/Test.sol";
 import "../src/MatchingGame.sol"
 
-contract MatchingGameTest is Test {}
+contract MatchingGameTest is Test {
+    function setUp()public {
+    
+        // chain of emoji numbers provided by the game logic
+        string memory answerChain = "12345678910";
+
+        // random salt to be added to the answer 
+        // it will make it harder to retrieve the answer 
+
+        bytes32 salt = bytes32("2163126323687461423612846");
+    }
+}
