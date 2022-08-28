92c9e840853654bb9eee9fe6e618f64cab045fcc
Kamil Chmielewski <chmielewski.kamil45@gmail.com>
2022-08-28 12:46:52 +0200

    docs(game-instance): update @author comment

--- foundry/src/MatchingGame.sol
+++ foundry/src/MatchingGame.sol
@@ -2,7 +2,7 @@
 pragma solidity 0.8.13;
 
 /**
- * @author Find us on twitter: @MaksymMalicki, @kamilchmielu
+ * @author Contact us on twitter: @MaksymMalicki, @kamilchmielu
  * @title Match the icon game
  * @notice This game is inspired by the great game of Dobble
  */
