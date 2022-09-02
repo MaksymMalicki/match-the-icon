088ea32f831594a3fc73cec7e35437d6f7ed9a96
Maksym Malicki <maksym.malicki.2115@gmail.com>
2022-09-02 10:08:12 +0100

    Added web3props interface and metamask presence checking

--- front-end/src/app/routing-modules/multiplayer-game/web3-auth.service.ts
+++ front-end/src/app/routing-modules/multiplayer-game/web3-auth.service.ts
@@ -1,23 +1,29 @@
-import { Injectable } from '@angular/core';
-import { ethers } from 'ethers';
+import {Injectable} from '@angular/core';
+import {ethers} from 'ethers';
+import {web3props} from "../../shared-interfaces/web3props.interface";
 
 @Injectable({
   providedIn: 'root'
 })
 export class Web3AuthService {
 
-  public web3Props = { provider: null, signer: null, account: null, chainId: null}
+  public web3Props: web3props = {provider: null, signer: null, account: null, chainId: null}
 
   public connectToMetamask(): void {
+    if ((!window as any).ethereum) {
+      return;
+    }
     let provider = new ethers.providers.Web3Provider((window as any).ethereum, 'any');
 
-    const connection = async (): Promise<void> => {
+    const connection = async (): Promise<web3props> => {
       await provider.send('eth_requestAccounts', []);
       const signer = provider.getSigner();
       const account = await signer.getAddress();
       const chainId = await signer.getChainId();
-      this.web3Props = {provider, signer, account, chainId}
+      return {provider, signer, account, chainId};
     }
-    connection();
+    connection().then(
+      r => this.web3Props = r,
+    );
   }
 }
